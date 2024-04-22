22d02a4d016da66425fa9507ada45b8876371586
julioD <juliougd@hotmail.com>
2024-04-22 12:31:51 -0300

    agrega configuracion en app.module para deploy en produccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,21 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl:
+          process.env.STAGE === 'prod' ? { rejectUnauthorized: false } : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
-      database:process.env.DB_NAME,
-      username:process.env.DB_USERNAME,
-      password:process.env.DB_PASSWORD,
-      autoLoadEntities:true,
-      synchronize:true //  En producción debe ir en false
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, //  En producción debe ir en false
     }),
     ProductsModule,
     CommonModule,
